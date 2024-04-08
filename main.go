7501079278a6e6fab3d93b5b8289fea3e95f8389
Nathan G. Santos <nathangar01@gmail.com>
2024-04-08 19:33:35 -0300

    feat: reading the input from a file

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	MAX_SIZE = 1024
+)
+
+func checkError(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readFile(path string) string {
+	file, err := os.Open(path)
+	checkError(err)
+	defer file.Close()
+
+	data := make([]byte, MAX_SIZE)
+	count, err := file.Read(data)
+	checkError(err)
+
+	return string(data[:count])
+}
 
 func main() {
-	fmt.Println("Hello World")
+	data := readFile("input.txt")
+	fmt.Println(data)
 }
